16c382b52f8bbefa2315dae565821d2b3587361e
MineCRAFT-H4cker <tanyukaimatthias@gmail.com>
2025-06-11 03:01:12 +0000

    Jun 11, 2025, 3:01 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -39,8 +39,8 @@ print("the answer is "+str(answer))
 password="passme"
 guess=input("what is the pasword")
 if guess==password:
-    if guess==password:
-        print("correct")
+    
+   print("correct")
 
 
 

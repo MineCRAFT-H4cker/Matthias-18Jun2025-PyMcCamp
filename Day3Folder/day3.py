931a093e4cbcf08fc4e2078ca6c216d742248c31
MineCRAFT-H4cker <tanyukaimatthias@gmail.com>
2025-06-11 02:21:17 +0000

    Jun 11, 2025, 2:21 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -11,7 +11,8 @@ yourage=int(yourage)+3#convert string to a number to do a math operation
 print("I am"+str(yourage)+"this year")#convert number to string to print out a sentence
 ########################################################################
 # Task 2
-yourname=input("whatis your name")
+yourname=input("what is your name")
+title=("")
 
 
 

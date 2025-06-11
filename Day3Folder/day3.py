3072b8bd2b3642ee316e06c7f984c63523441fc0
MineCRAFT-H4cker <tanyukaimatthias@gmail.com>
2025-06-11 02:23:46 +0000

    Jun 11, 2025, 2:23 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -13,7 +13,7 @@ print("I am"+str(yourage)+"this year")#convert number to string to print out a s
 # Task 2
 yourname=input("what is your name")
 print("hello"+yourname)       
-yourtitle=("are you the king")
+yourtitle=("are you the king?")
 
 
 

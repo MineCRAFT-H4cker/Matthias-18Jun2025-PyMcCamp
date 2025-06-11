05f1ed9fad8faa5073e4f4c5d2f8989bd62d1f15
MineCRAFT-H4cker <tanyukaimatthias@gmail.com>
2025-06-11 01:53:18 +0000

    Jun 11, 2025, 1:53 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,7 +1,7 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day1")
-
+print("I AM STEVE")
+print("I AM NOT STEVE")
 ########################################################################
 # Task 1:
 
@@ -9,33 +9,70 @@ print("hello from day1")
 
 ########################################################################
 # Task 2:
-
-
-
+print("Hello I am matthias")
+print("I am 12 years old")
+print("I attend Xinmin secondary school")
+print("if I had 2000 dollars,I would invest or buy robux")
+# print("muahahahaaahahahaaaaha")
 ########################################################################
 # Task 3:
-
-
-
+wallet=1000000000000000000000000
+bankaccount=100000000000000000000000000000
+animal1="tiger"
+animal2="turkey with mauser"
+animal3="bombrito bandito"
+print(animal1)
+print(animal2)
+print(animal3)
 ########################################################################
 # Task 4:
-
-
-
+x=2
+y=3
+print(x+y)
+print(x*y)
+print(x/y)
+print(x-y)
 ########################################################################
 # Task 5:
-
-
-
+name="matthias"
+message=" cooked pork"
+print("my name is " +name)
+print("I like to eat"+message)
 ########################################################################
 # Task 6:
-
-
+def intro():
+    print("Hello I am matthias")
+    print("I am 12 years old")
+    print("I attend Xinmin secondary school")
+    print("if I had 2000 dollars,I would invest or buy robux")
+intro()
 
 ########################################################################
 # Task 7:
-
-
+def addition(number1,number2):
+    print(number1+number2)    
+addition(10,1000)
 
 ########################################################################
 # Additional exercises:
+for count in range(100):
+    print("I will not sling mud at my friends")
+name="Matthias"
+for letter in name:
+    print("Give me a "+letter+"!")
+
+print(name+"is the best!")
+for count in range(1,11):
+    print(count)
+for count in range(5,33):
+    print(count)
+for count in range(100,0,-1):
+    print (count)
+
+
+
+
+
+
+
+

d8da9178048592d225fd51576666295b8381d6f9
MineCRAFT-H4cker <tanyukaimatthias@gmail.com>
2025-06-12 02:14:31 +0000

    Jun 12, 2025, 2:14 AM

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -7,7 +7,7 @@ print("hello from day4")
 counter=0
 while counter<5:
     print(counter)
-    
+    counter=counter+1
 
 ########################################################################
 # Task 2:

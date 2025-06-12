ae02583ad8a8ccdbf06abd4edd94b6610beb2ce1
MineCRAFT-H4cker <tanyukaimatthias@gmail.com>
2025-06-12 02:02:51 +0000

    Jun 12, 2025, 2:02 AM

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -7,6 +7,7 @@ print("hello from day4")
 import random 
 for count in range(10):
     randomnumber=random.randint(1,1000)
+    print(randomnumber)
 
 
 ########################################################################
